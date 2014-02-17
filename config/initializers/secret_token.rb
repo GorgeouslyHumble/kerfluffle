f7ebb0f6a1895b87b9391c5c474c633c40184f45
gorgeouslyhumble <dr.frankinfurter@gmail.com>
2014-02-17 15:40:37 -0800

    moved project to rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kerfluffle::Application.config.secret_key_base = '3f4ae1d0b5e815e843052e26c46044c0d5b0f432182727fe26ff220da4e2da332311a1a077f09e28f2ed15d65e01661165fbdc25d604093b7a58d2d95b97e3bc'
