ade56775ea3cb204d6aaf8249c17c410070bf6a3
howdoicomputer <tyler@insidevault.com>
2014-03-02 17:52:37 -0800

    added teemo method to controller and specified as a helper method and then called that method from the view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,22 @@
+require 'lol'
+require 'pp'
+
 class WelcomeController < ApplicationController
+
+  @@client = Lol::Client.new "6a4e5fbd-b36b-4816-bd3f-f2b3b2086fc5"
+
+  def return_teemo
+    champions = @@client.static.champion.get(champData: 'all')
+
+    champions.each do |c|
+      if c.name.match('Teemo')
+        @teemo = c
+      end
+    end
+
+    @teemo
+  end
+
+  helper_method :return_teemo
+
 end
